0cfa66c85d35422d3d253bcb816c4b83c73ff6c9
SevvalAcet <83153151+SevvalAcet@users.noreply.github.com>
2023-02-15 21:22:36 +0300

    AuditableEntity oluşturuldu.

---
+++ Entities/Concrete/BaseEntities/AuditableEntity.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+
+namespace Entities.Concrete.BaseEntities
+{
+    public class AuditableEntity : BaseEntity, ICreatedEntity, IUpdatedEntity, IDisplayEntity
+    {
+        public int CreatedUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int IUpdatedUserId { get ; set; }
+        public DateTime? IUpdatedDate { get; set ; }
+        public int DisplayOrder { get ; set; }
+        public bool IsDisplay { get; set ; }
+    }
+}
