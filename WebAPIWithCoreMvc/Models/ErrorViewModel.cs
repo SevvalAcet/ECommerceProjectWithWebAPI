fa1c2aee0906c01a701343e731981f1d007a6b8e
SevvalAcet <83153151+SevvalAcet@users.noreply.github.com>
2023-02-15 20:17:29 +0300

    Add project files.

---
+++ WebAPIWithCoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAPIWithCoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
