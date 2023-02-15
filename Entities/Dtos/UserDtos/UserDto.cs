3d6b194e2b6d17d977b56444c7f66718c76273f5
SevvalAcet <83153151+SevvalAcet@users.noreply.github.com>
2023-02-15 21:24:19 +0300

    UserDto oluşturuldu

---
+++ Entities/Dtos/UserDtos/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+
+namespace Entities.Dtos.UserDtos
+{
+    public class UserDto:IDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
