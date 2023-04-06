fb824f0119c8013e513dab7dc8393ccf45a1f506
SevvalAcet <83153151+SevvalAcet@users.noreply.github.com>
2023-04-06 23:39:00 +0300

    Swagger & JwtToken Extension

--- Core/Extensions/JwtTokenExtensions.cs
+++ Core/Extensions/JwtTokenExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using Core.Utilities.Security.Token;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 namespace Core.Extensions
