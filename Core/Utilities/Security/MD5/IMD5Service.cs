29884a34aefaaeab4fa719b45476bbb0b4803fef
SevvalAcet <83153151+SevvalAcet@users.noreply.github.com>
2023-04-13 11:46:32 +0300

    Hash(Sha256,Sha512) ve MD5 Şifreleme

---
+++ Core/Utilities/Security/MD5/IMD5Service.cs
@@ -0,0 +1,25 @@
+﻿namespace Core.Utilities.Security.MD5
+{
+    public interface IMD5Service
+    {  /// <summary>
+       /// Metni MD'e Dönüştür
+       /// </summary>
+       /// <param name="text">string text</param>
+       /// <returns>string text</returns>
+        string ConvertTextToMD5(string text);
+
+        /// <summary>
+        /// MD5 ile Şifreleme
+        /// </summary>
+        /// <param name="text">string text</param>
+        /// <returns>string text</returns>
+        string Encrypt(string text);
+
+        /// <summary>
+        /// Şifresini Çöz (MD5)
+        /// </summary>
+        /// <param name="encryptedValue">string MD5</param>
+        /// <returns>string text</returns>
+        string Decrypt(string encryptedValue);
+    }
+}
